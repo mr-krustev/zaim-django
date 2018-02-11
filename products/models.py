6f4da883e731fac7ebaa69b9621d1d7afa0bf6e4
meetov <mitov.mario94@gmail.com>
2018-02-11 17:04:58 +0200

    Added Product Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    # these three are a must I think, max_length can be adjusted
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    price = models.FloatField()
+
+    # asked Yonca, she said there can be more than 1 of the same product
+    quantity = models.IntegerField(default=0)
+
+    # maybe useful for statistics, e.g. when was that product
+    # last purchased/discounted/viewed
+    last_modified = models.DateField(auto_now=True)
+
+    product_types = (
+        ('Clothes', (
+                ('T', 'Tops'),
+                ('B', 'Bottoms'),
+                ('L', 'Lingerie'),
+        )),
+        ('Accesories', (
+                ('J', 'Jewelry'),
+                ('S', 'Shoes'),
+                ('B', 'Bags'),
+        )),
+    )
+
+    product_type = models.CharField(max_length=3,
+                                    choices=product_types,
+                                    default=product_types[0][0][0])
+
+# https://docs.djangoproject.com/en/2.0/ref/models/fields/#field-choices
